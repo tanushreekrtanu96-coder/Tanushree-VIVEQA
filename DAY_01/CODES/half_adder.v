4fdcc52d8d6c0c7569533644488972a8b62fcccb
Tanushree K.R <tanushreekrtanu96@gmail.com>
2026-07-20 10:39:23 +0530

    Update half_adder.v

--- DAY_01/CODES/half_adder.v
+++ DAY_01/CODES/half_adder.v
@@ -6,19 +6,3 @@ xor(sum,a,b);
 and(carry,a,b);
 
 endmodule
-
-module half_adder_tb();
-reg a,b;
-wire sum,carry;
-
-half_adder dut(a,b,sum,carry);
-
-initial begin
-a=1'b0;b=1'b0;
-#5 a=1'b0;b=1'b1;
-#5 a=1'b1;b=1'b0;
-#5 a=1'b1;b=1'b1;
-#5 $finish;
-end
-
-endmodule
